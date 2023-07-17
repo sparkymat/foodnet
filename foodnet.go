8a57a34611bb94925094210ac77f208418c56034
Ajith Hussain <ajith.hussain@fastmail.com>
2023-07-17 19:24:37 +1000

    Added health endpoint

--- foodnet.go
+++ foodnet.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/sparkymat/foodnet/internal/route"
+)
+
 func main() {
+	e := echo.New()
+
+	route.Setup(e)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
